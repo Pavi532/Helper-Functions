86c2593590f4a23c31a80a027a1d3b15a218c211
Udara Deraniyagala <44343688+Pavi532@users.noreply.github.com>
2023-02-19 16:12:32 +0530

    Initial commit

---
+++ waitForElement.js
@@ -0,0 +1,23 @@
+//From https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+
+async function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+const elm = await waitForElm('.some-class');
